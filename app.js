70c260617aa540eada54ed22eab8e975368eab94
Maxime <maxime.antoine2@gmail.com>
2016-12-03 01:41:18 +0800

    changed connection logic to require the playerId from the client instead of generating it

--- app.js
+++ app.js
@@ -1,78 +1,82 @@
 (function(){
 	var io = require('socket.io')(process.env.PORT || 3000);
-	var shortId = require('shortid');
 	
 	console.log('server started');
 	
 	var players = [];
 	
 	io.on('connection', function(socket){
-		//create player obj
-		var clientId = shortId.generate();
-		var player = {
-			id: clientId,
-			targetPosition: {
-				x: 0, y: 0, z: 0
-			}
-		};
-		players[clientId] = player;
-	
-		//------------------------------ spawn logic -------------------------------------
-		//emit spawn and request position to all other connected players
-		console.log('client connected - broadcasting spawn - id: ' + clientId);
+		//require registration (playerId)
+		console.log('client connected - required registration');
+		socket.emit('register');
+		
+		socket.on('onRegister', function(data){
+			//create player obj
+			var clientId = data.playerId;
+			var player = {
+				id: clientId,
+				targetPosition: {
+					x: 0, y: 0, z: 0
+				}
+			};
+			players[clientId] = player;
+		
+			//------------------------------ spawn logic -------------------------------------
+			//emit spawn and request position to all other connected players
+			console.log('client registered with id: ' + clientId + ' - broadcasting spawn');
 
-		socket.emit('register', { id: clientId});
-		socket.broadcast.emit('spawn', player);
-		socket.broadcast.emit('requestPosition');
-		
-		//emit spawn for each connected player
-		for (var playerId in players){
-			if (playerId != clientId) {
-				socket.emit('spawn', players[playerId]);
-				console.log('sending spawn new player for id: ' + playerId);
+			socket.broadcast.emit('spawn', player);
+			socket.broadcast.emit('requestPosition');
+			
+			//emit spawn for each connected player
+			for (var playerId in players){
+				if (playerId != clientId) {
+					socket.emit('spawn', players[playerId]);
+					console.log('sending spawn new player for id: ' + playerId);
+				}
 			}
-		}
-		
-		//---------------------------- event handlers ------------------------------------
-		socket.on('move', function(data){
-			data.id = clientId;
-			console.log('client moving to: ' + JSON.stringify(data));
 			
-			//update position
-			player.targetPosition.x = data.x;
-			player.targetPosition.y = data.y;
-			player.targetPosition.z = data.z;
+			//---------------------------- event handlers ------------------------------------
+			socket.on('move', function(data){
+				data.id = clientId;
+				console.log('client moving to: ' + JSON.stringify(data));
+				
+				//update position
+				player.targetPosition.x = data.x;
+				player.targetPosition.y = data.y;
+				player.targetPosition.z = data.z;
+				
+				socket.broadcast.emit('move', player);
+			});
 			
-			socket.broadcast.emit('move', player);
-		});
-		
-		socket.on('updatePosition', function(data){
-			data.id = clientId;
-			console.log('Update position: ' + JSON.stringify(data));
+			socket.on('updatePosition', function(data){
+				data.id = clientId;
+				console.log('Update position: ' + JSON.stringify(data));
 
-			socket.broadcast.emit('updatePosition', data);
-		});
-		
-		socket.on('follow', function(data){
-			data.id = clientId;
-			console.log('Follow request: ' + JSON.stringify(data));
+				socket.broadcast.emit('updatePosition', data);
+			});
 			
-			socket.broadcast.emit('follow', data);
-		});
-		
-		socket.on('attack', function(data){
-			console.log('attack request: ', data);
-			data.id = clientId;
+			socket.on('follow', function(data){
+				data.id = clientId;
+				console.log('Follow request: ' + JSON.stringify(data));
+				
+				socket.broadcast.emit('follow', data);
+			});
 			
-			io.emit('attack', data);
-		});
-		
-		//on player disconnect
-		socket.on('disconnect', function(){
-			console.log('client disconnected - id: ' + clientId);
-			delete players[clientId];
+			socket.on('attack', function(data){
+				console.log('attack request: ', data);
+				data.id = clientId;
+				
+				io.emit('attack', data);
+			});
 			
-			socket.broadcast.emit('clientDisconnected', { id: clientId });
-		});
+			//on player disconnect
+			socket.on('disconnect', function(){
+				console.log('client disconnected - id: ' + clientId);
+				delete players[clientId];
+				
+				socket.broadcast.emit('clientDisconnected', { id: clientId });
+			});
+			});
 	});
 })();
